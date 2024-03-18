0b880c91c3e7dfe9c9fa0dc763255b236a5e927b
Aleksandr Fedorov <aldrfedorov97@mail.ru>
2024-03-18 09:09:51 +0300

    feat(order-core): builder for OrderStatus

--- order-core/src/main/java/com/example/ordercore/model/kafka/messages/OrderStatus.java
+++ order-core/src/main/java/com/example/ordercore/model/kafka/messages/OrderStatus.java
@@ -1,10 +1,12 @@
 package com.example.ordercore.model.kafka.messages;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.Instant;
 
 @Data
+@Builder
 public class OrderStatus {
     private String status;
     private Instant date;
